4739f326173994d08870fc906346c3b9e195e56b
Neerajcb <126134369+Neerajcb@users.noreply.github.com>
2024-08-10 14:39:30 +0530

    commitdata
    done

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['student_name', 'class_teacher_id', 'class', 'admission_date', 'yearly_fees'];
 
