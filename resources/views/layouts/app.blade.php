2d36967257ae3647931da29fa51a1b1554649f03
Neerajcb <126134369+Neerajcb@users.noreply.github.com>
2024-08-10 15:08:50 +0530

    pushdata
    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Bootstrap CSS -->
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Optional: Custom CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  
 </head>
 <body>
     <div id="app">
@@ -63,7 +68,16 @@
             @yield('content')
         </main>
     </div>
+      
+    <!-- DataTables JS -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script>
+            $(document).ready(function() {
+        $('#studentsTable').DataTable();
+    });
+    </script>
 </body>
 </html>
